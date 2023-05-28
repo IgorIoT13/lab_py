56610ef7e24b872bae79bdf028c60e4a6e8214e7
Igor <ihor.slodziak.ir.2022@lpnu.ua>
2023-05-28 17:19:53 +0300

    lab_9

--- model/university_garden.py
+++ model/university_garden.py
@@ -1,33 +1,31 @@
+"""Student Ihor IR-13 """
+
 from model.garden import Garden
 
 
-class University_garden(Garden):
+class UniversityGarden(Garden):
+    """Simulation real university garden"""
+
     def __init__(self, area: int = 0, flower: int = 0, count_sculpture: int = 0):
+        """Constructor"""
+
         super().__init__(area, flower)
-        self.__count_sculpture = count_sculpture
+        self._count_sculpture = count_sculpture
 
     def get_count_sculpture(self):
+        """return greenhouses to use it in code"""
+
         return self.__count_sculpture
 
     def has_orchard(self):
+        """return status orchard in garden"""
+
         return False
 
     def has_vegetable_garden(self):
+        """return status vegetable garden in garden"""
+
         return False
 
-    @staticmethod
-    def plant_flower(flower):
-        super().plant_flower(flower)
-
-    @staticmethod
-    def remove_flower(count):
-        super().remove_flower(count)
-
-    @staticmethod
-    def add_vegetable_region(count):
-        """add area to garden"""
-        super().add_vegetable_region(count)
-
     def __str__(self):
-        return super().__str__() + f", sculpture: {self.__count_sculpture}" + "\n"
-
+        return super().__str__() + f", sculpture: {self._count_sculpture}" + "\n"
